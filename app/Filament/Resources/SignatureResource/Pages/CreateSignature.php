1f0299fb29ed14c3693996f2357b0dfeda29d7e5
EgiKelo9 <aryadanabaraja@gmail.com>
2026-01-21 21:00:34 +0800

    fixing bug unique link

--- app/Filament/Resources/SignatureResource/Pages/CreateSignature.php
+++ app/Filament/Resources/SignatureResource/Pages/CreateSignature.php
@@ -17,7 +17,7 @@ class CreateSignature extends CreateRecord
     {
         $signature = Signature::create([
             'nomor' => $data['nomor'],
-            'unique_link' => str_replace('.', '-', uniqid(env('APP_URL') . '/signature/', true)),
+            'unique_link' => env('APP_URL') . str_replace('.', '-', uniqid('/signature/', true)),
             'file' => $data['file'],
             'keperluan' => $data['keperluan'],
             'id_user' => $data['id_user'],
